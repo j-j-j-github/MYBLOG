b73223cdc09ff2d7a13ee11b792d1013aa1b416d
j-j-j-github <jeevaljolly@gmail.com>
2025-11-02 21:46:34 +0530

    Added image to Intro Screen.

--- myblog/settings.py
+++ myblog/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'blog.context_processors.latest_posts_processor',
+                'django.template.context_processors.media',
             ],
         },
     },
